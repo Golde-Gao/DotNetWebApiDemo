78590c85ef94428a8ff9a4b9fe9fc77c11833fac
gsixin <gsixin@live.com>
2021-09-25 06:58:40 +0800

    add test proj

---
+++ LancooDemoInActionTests/Controllers/TestTaskControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LancooDemoInAction.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LancooDemoInAction.Controllers.Tests
+{
+    [TestClass()]
+    public class TestTaskControllerTests
+    {
+        [TestMethod()]
+        public void GetTestTasksTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
